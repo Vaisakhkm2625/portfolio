a2938179663f8fafdf489685cdfc8f42a0a65f1f
Vaisakh K M <vaisakhkm2625@gmail.com>
2025-10-27 05:38:56 +0530

    add links to navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -2,9 +2,9 @@
 <nav class="border-b border-[#87ff8755] mb-[21px]">
     <ul class="flex justify-between items-center list-none overflow-hidden px-2">
         <div class="flex space-x-2">
-            <li class="float-left"> <a href="#" class="block text-center text-[var(--color-green)] py-[14px] px-[13px] no-underline hover:text-[var(--color-primary)]">Home</a> </li>
-            <li class="float-left"> <a href="#" class="block text-center text-[var(--color-accent)] py-[14px] px-[13px] no-underline hover:text-[var(--color-primary)]">Nixify</a> </li>
-            <li class="float-left"> <a href="#" class="block text-center text-[var(--color-secondary)] py-[14px] px-[13px] no-underline hover:text-[var(--color-secondary)]">Notes</a> </li>
+            <li class="float-left"> <a href="https://nixifying.my.id/" class="block text-center text-[var(--color-green)] py-[14px] px-[13px] no-underline hover:text-[var(--color-primary)]">Home</a> </li>
+            <li class="float-left"> <a href="https://nixos.org/" class="block text-center text-[var(--color-accent)] py-[14px] px-[13px] no-underline hover:text-[var(--color-primary)]">Nixify</a> </li>
+            <li class="float-left"> <a href="https://notes.nixifying.my.id/" class="block text-center text-[var(--color-secondary)] py-[14px] px-[13px] no-underline hover:text-[var(--color-secondary)]">Notes</a> </li>
         </div>
         <div>
             <li class="float-left"> <a href="#" class="block text-center text-[var(--color-foreground)] py-[14px] px-[13px] no-underline hover:text-[var(--color-green)]">Contact</a>  </li>
