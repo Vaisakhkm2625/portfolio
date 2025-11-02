ad1ebeab95dbd3edbc963f60e9258c44682d668a
Vaisakh K M <vaisakhkm2625@gmail.com>
2025-11-03 04:38:00 +0530

    view counter to navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,3 +1,8 @@
+<script>
+
+
+import ViewCounter from '$lib/components/ViewCounter.svelte';
+</script>
 
 <nav class="border-b border-[#87ff8755] mb-[21px]">
     <ul class="flex justify-between items-center list-none overflow-hidden px-2">
@@ -7,6 +12,9 @@
             <li class="float-left"> <a href="https://notes.nixifying.my.id/" class="block text-center text-[var(--color-secondary)] py-[14px] px-[13px] no-underline hover:text-[var(--color-secondary)]">Notes</a> </li>
         </div>
         <div>
+
+            
+            <li class="float-left"> <a href="#" class="block text-center text-[var(--color-foreground)] py-[14px] px-[13px] no-underline hover:text-[var(--color-green)]"><ViewCounter/></a>  </li>
             <li class="float-left"> <a href="#" class="block text-center text-[var(--color-foreground)] py-[14px] px-[13px] no-underline hover:text-[var(--color-green)]">Contact</a>  </li>
         </div>
     </ul>
